ede2cddf9a273ec1ff5737e12d808bd217cd3b27
LucasAPV <lucasaugustopereiravilela@gmail.com>
2025-12-09 19:57:09 -0300

    Adding BackSpace and Tab

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,48 @@
 use crossterm::{
-    event::{Event, KeyEventKind, read},
+    QueueableCommand, cursor::MoveTo, event::{Event, KeyEventKind, read}, terminal::{Clear, ClearType}
 };
-use std::{
-    io::{self, Write, stdout},
+use std::io::{self, Write, stdout};
+
+//pub mod App;
+#[allow(non_snake_case)]
+pub mod App;
+use App::{
+    App as application, 
+    Text, 
+    FILE_PATH, 
+    Modes
 };
 
-mod app;
-use app::*;
 fn main() -> io::Result<()> {
     let mut text = Text::new();
     let s = text.retrive(FILE_PATH.to_string());
     let mut stdout = stdout();
-    let mut app = app::App {
+    let mut app = application {
         mode: Modes::Normal,
+        line: 0,
+        col: 0
     };
 
+    stdout.queue(Clear(ClearType::All)).unwrap();
+    stdout.flush()?;
     if s.value != "".to_string() {
         println!("{}", s.value);
         stdout.flush()?;
     }
-
+    stdout.queue(MoveTo(0u16,0u16)).unwrap();
+    stdout.flush()?;
     loop {
         match app.mode {
             Modes::Normal => match read()? {
                 Event::Key(key_event) => {
                     if key_event.kind == KeyEventKind::Press {
                         text.value =
-                            app::App::handle_normal_input(key_event, &mut app, &text, &mut &stdout)
+                            application::
+                                handle_normal_input
+                                    (
+                                        key_event, &mut app, 
+                                        &text, &mut &stdout
+                                    )
                                 .unwrap();
                     }
                 }
@@ -34,7 +50,12 @@ fn main() -> io::Result<()> {
             },
             Modes::Insert => match read()? {
                 Event::Key(key_event) => {
-                    app::App::handle_insert_input(key_event, &mut app, &mut text, &mut &stdout);
+                    application::
+                        handle_insert_input
+                            (
+                                key_event, &mut app, 
+                                &mut text, &mut &stdout
+                            );
                 }
                 _ => {}
             },
